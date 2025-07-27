95eb111bf11174585c5ea8271b3c805557e5ab0c
Deepak_lak <dkumar17071@gmail.com>
2025-07-28 01:00:18 +0530

    mobile menu not opening bug fixed

--- frontend_react_2/src/components/Navbar/Navbar.jsx
+++ frontend_react_2/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./navbar.scss";
 import { HiMenuAlt4, HiX } from "react-icons/hi";
 import { motion } from "framer-motion";
@@ -6,11 +6,16 @@ import { Images } from "../../constants";
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
+  useEffect(() => {
+    document.body.style.overflow = toggle ? "hidden" : "auto";
+  }, [toggle]);
+
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
-        <img src={Images.logo2} alt="" />
+        <img src={Images.logo2} alt="logo" />
       </div>
+
       <ul className="app__navbar-links">
         {["home", "about", "work", "skills", "testimonials", "contact"].map(
           (item) => (
@@ -21,16 +26,28 @@ const Navbar = () => {
           )
         )}
       </ul>
+
       <div className="app__navbar-menu">
         <HiMenuAlt4 onClick={() => setToggle(true)} />
+
         {toggle && (
           <motion.div
-            whileInView={{ x: [300, 0] }}
-            transition={{ duration: 0.85, ease: "easeOut" }}
+            className="app__navbar-menu-content"
+            initial={{ x: 300, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            exit={{ x: 300, opacity: 0 }}
+            transition={{ duration: 0.5, ease: "easeOut" }}
           >
             <HiX onClick={() => setToggle(false)} />
             <ul>
-              {["home", "about", "work", "skills", "contact"].map((item) => (
+              {[
+                "home",
+                "about",
+                "work",
+                "skills",
+                "testimonials",
+                "contact",
+              ].map((item) => (
                 <li key={item}>
                   <a href={`#${item}`} onClick={() => setToggle(false)}>
                     {item}
